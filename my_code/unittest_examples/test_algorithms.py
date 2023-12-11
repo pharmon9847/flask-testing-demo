0c53ad9acd16388337ebb5fea8ea530c02d86ea9
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-12-11 15:40:28 -0500

    pushed before getting kids from school

--- my_code/unittest_examples/test_algorithms.py
+++ my_code/unittest_examples/test_algorithms.py
@@ -5,3 +5,16 @@ class AlgorithmsTestCase(TestCase):
     def test_reverse(self):
         self.assertEqual(reverse_str('hello'), 'olleh')
         self.assertEqual(reverse_str('Apple'), 'elppA')
+        
+    def test_is_palindrome(self):
+        self.assertTrue(is_palindrome('racecar'))
+        self.assertTrue(is_palindrome('kayak'))
+        # should ignore casing
+        self.assertTrue(is_palindrome('Racecar'))
+        self.assertFalse(is_palindrome('taco'))
+        
+    def test_factorial(self):
+        self.assertEqual(factorial(5), 120)
+        self.assertEqual(factorial(3), 6)
+        self.assertRaises(ValueError, factorial, -5)
+        self.assertRaises(ValueError, factorial, 4.3)
